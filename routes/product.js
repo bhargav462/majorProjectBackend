8de7a05f986a4ce471fa6d8025d78c7c0e0872d5
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2021-04-27 16:46:00 +0530

    changing multer

--- routes/product.js
+++ routes/product.js
@@ -4,17 +4,8 @@ const mongoose = require('mongoose')
 const Products = mongoose.model("products");
 
 const multer = require('multer');
-var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './public/uploads')
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now())
-    }
-});
-const upload = multer({
-    storage: storage
-})
+
+const upload = multer();
 
 app.get('/', async (req,res) => {
     const products = await Products.find();
