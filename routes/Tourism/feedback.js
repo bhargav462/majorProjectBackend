4ef783d958004032a56d2cba99a324ebe393b206
Nakkina Bhargav <43701429+bhargav462@users.noreply.github.com>
2021-06-22 22:52:59 +0530

    tourism

--- routes/Tourism/feedback.js
+++ routes/Tourism/feedback.js
@@ -3,12 +3,14 @@ const router = express.Router()
 const Feedback = require('./../../models/Feedback/feedback')
 
 router.post('/form',async (req,res) => {
+    console.log("Body",req.body)
     try{
         const {name,place} = req.body
         const feedback = new Feedback();
         feedback.name = name
         feedback.place = place
         feedback.feedback = req.body.feedback
+        console.log("feedback",feedback)
         await feedback.save()
         res.send();
     }catch(e){
